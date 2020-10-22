877601b4000f1c7d07e859441c20c022dbe0d045
Yuri <yuri@MacBook-Pro-dns.local>
2020-10-23 00:00:10 +0300

    My homework

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,10 @@
 function sum(a, b) {
-  /* ваш код */
+  if (typeof a == 'number' && typeof b == 'number') 
+  {
+    return a + b;
+  }
+  throw new TypeError('TypeError');
 }
+
 
 module.exports = sum;
